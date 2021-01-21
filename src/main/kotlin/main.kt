f8368a2ab2d68f1d16130f28d99f9c90fd9b1895
TurcnaninovUV <pro-h92@yandex.ru>
2021-01-21 14:24:20 +0300

    исправил замечания

--- src/main/kotlin/main.kt
+++ src/main/kotlin/main.kt
@@ -1,24 +1,25 @@
 fun main() {
     println("Введите сумму в рублях которую вы хотели перевести!")
     var input = readLine()!!.toInt() * 100
-    val result = transaction("MasterCard", 100000, input)
-    val result1 = transaction(amountTransfers = 50000, amount = input)
-    val result2 = transaction("Мир", amount = input)
-    println("Сумма перевода будет составлять $result коп. ( ${result / 100} руб. )")
-    println("Сумма перевода будет состовлять $result1 коп. ( ${result1 / 100} руб. )")
-    println("Сумма перевода будет состовлять $result2 коп. ( ${result2 / 100} руб. )")
+    transaction("MasterCard", 100000, input)
+    transaction(amountTransfers = 50000, amount = input)
+    transaction("Мир", amount = input)
 }
 
-fun transaction(card: String = "VKpay",
-                amountTransfers: Int = 0,
-                amount: Int
-): Int {
+fun transaction(card: String = "VKpay", amountTransfers: Int = 0, amount: Int) {
     var cost = amount
     when (card) {
-        "MasterCard", "Maestro" -> if (amountTransfers < 75000) cost
-        else cost = (amount * 0.94 - 2000).toInt()
+        "MasterCard", "Maestro" -> cost = comissionForMastercardAndMaestro(amountTransfers, amount)
         "VKpay" -> cost
-        "Visa", "Мир" -> if ((amount * 0.075) < 3500) cost = amount - 3500 else cost = (amount * 0.925).toInt()
+        "Visa", "Мир" -> cost = comissionForVisaAndMir(amount)
     }
-    return cost
+    println("Сумма перевода будет состовлять $cost коп. ( ${cost / 100} руб. )")
+}
+
+fun comissionForMastercardAndMaestro(amountTransfers: Int = 0, amount: Int): Int {
+    return if (amountTransfers < 75000) amount else (amount * 0.94 - 2000).toInt()
+}
+
+fun comissionForVisaAndMir(amount: Int): Int {
+    return if ((amount * 0.075) < 3500) amount - 3500 else (amount * 0.925).toInt()
 }
